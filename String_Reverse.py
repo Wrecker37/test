8e63fc14e1f30c3739916d8f8afbff49e621ca18
Wrecker37 <jairo.pedroza78@gmail.com>
2022-07-14 11:51:55 -0500

    copilot introduced to String_Reverse.py

--- String_Reverse.py
+++ String_Reverse.py
@@ -1,9 +1,32 @@
 import pygame
 
-for i in range(5):
-    print("Hello World")
 
+"""print hellow world 5 times"""
+def print_hello_world():
+    for i in range(5):
+        print("Hello World")
+
+"""ask user for a string and print backwards"""
+def reverse_string():
+    string = input("Enter a string: ")
+    string = string[::-1]
+    print(string)
+
+"""ask user for a string and determine if it is a palindrome"""
+def is_palindrome():
+    string = input("Enter a string: ")
+    if string == string[::-1]:
+        print("Yes, it is a palindrome")
+    else:
+        print("No, it is not a palindrome") 
+
+
+print_hello_world()
+reverse_string()
+
+"""
 string = input("Enter a string: ")
+
 
 print("input string: " + string)
 
@@ -25,3 +48,4 @@ reversedString = ''.join(stringToList)
 print("\nreversed string: " + reversedString)
 
 "comment"
+"""
