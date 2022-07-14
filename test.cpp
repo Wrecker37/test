3bc370046bb78907c5a636051c468a581ebd2a73
Wrecker37 <jairo.pedroza78@gmail.com>
2022-07-14 12:07:46 -0500

    change to test.cpp

--- test.cpp
+++ test.cpp
@@ -14,7 +14,15 @@ public:
 
 
 
-
+//write a function that prints a tree in preorder
+void printPreorder(Node* root) {
+    if (root == NULL) {
+        return;
+    }
+    std::cout << root->value << " ";
+    printPreorder(root->left);
+    printPreorder(root->right);
+}
 
 //make a function that reverses a binary tree   (recursive)
 void reverse(Node* root) {
@@ -29,8 +37,24 @@ void reverse(Node* root) {
 }
 
 int main(){
+    //generate a tree
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+    
+    //print the tree preorder
+    printPreorder(root);
 
+    //reverse the tree
+    reverse(root);
 
-    std::cout << "Hello World!" << std::endl;
+    //insert new line and then print the tree preorder
+    std::cout << std::endl;
+    printPreorder(root);
+
     return 0;
 }
